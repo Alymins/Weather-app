0516f37d7c081b5aeca716be5ac9cde07ac12adf
Harrison Marques Freitas <alymins@protonmail.com>
2021-06-04 16:12:13 -0300

    Update version - localStorage + close card

--- js/weatherApi.js
+++ js/weatherApi.js
@@ -1,3 +1,6 @@
+var cities = new Array();
+var citiesStorage = window.localStorage;
+
 function createCard(data) {
     if ($("#response .container .row:last div.card").length >= 4){
         $("#response .container").append("<div class='row row-sizing'/>");
@@ -13,36 +16,30 @@ function createCard(data) {
 
     const icon = `wi-owm-${timeDay}-${weather[0].id}`
     
-    $(".row:last").append("<div class='col-md-3 col-sm-6'/>");
+    $(".row:last").append(`<div id="${name.replace(" ", "")}" class='col-md-3 col-sm-6'/>`);
     $(".row:last div.col-sm-6.col-md-3:last").append("<div class='card card-espace'/>");
     $("div.card:last").append("<div class='card-body'/>");
-    $("div.card-body:last").append(`<div class="card-title"><p class="city-name">${name} <span><sup>${sys.country}</sup></span></p></div>`);
+    $("div.card-body:last").append(`<div class="card-title"><button onclick="closeCard('${name}')" class="close">x</button><p class="city-name">${name} <span><sup>${sys.country}</sup></span></p></div>`);
     
     $("div.card-body:last").append(`<div class="card-img temp-icon"><i class="wi ${icon}"></i></div>`);
     $("div.card-body:last").append(`<div class="card-text city-temp">${Math.round(main.temp)}<span><sup>°C</sup></span></div>`);
     
     $("div.card-body:last").append(`<div class="card-text temp-description">${weather[0].description}</div>`);
 
- }
-var cities = new Array();
+};
 
-$(function () {
-    $("button#send").click(function (e) { 
-        e.preventDefault();
-        const city = $("#city").val();
-        
-        // Clear 
-        $("#city").val("")
-        $("#error").text("");
-
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6fdf265efb2206615819bdbb49de9dbc&units=metric`).then(
-            response => {
-                return response.json();
-            }).then(data => {   
+function requestWeatherApi(cityName) {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=6fdf265efb2206615819bdbb49de9dbc&units=metric`
+    fetch(url).then(
+        response => {
+            return response.json();
+        }).then(
+            data => {
                 if (data.cod == 200 && data.cod < 400){
                     if (cities.indexOf(data.name) < 0){
                         createCard(data);
                         cities.push(data.name);
+                        citiesStorage.setItem("cities",cities)
                     }
                     else{
                         $("#error").text(`You already know the weather for ${data.name}.`);
@@ -52,8 +49,40 @@ $(function () {
                     $("#error").text("Please search for a valid city.");
                 };
 
-            }).catch(err => {
+            }
+        ).catch(
+            err =>{
                 console.log(err)
-            })
+            }
+        );
+};
+
+function closeCard(cityName){
+    if (cities.indexOf(cityName) >= 0){
+        const idCity = "#" + cityName.replace(" ","");
+        $(idCity).remove();
+        delete cities[cities.indexOf(cityName)];
+    };
+};
+
+$(function () {
+    var cityInStorage = citiesStorage.getItem("cities");
+    citiesStorage.removeItem("cities");
+    
+    if (cityInStorage != null){
+        cityInStorage.split(",").forEach(function (city) {
+            requestWeatherApi(city);
+        })
+    };
+
+    $("button#send").click(function (e) { 
+        e.preventDefault();
+        const city = $("#city").val();
+        
+        // Clear 
+        $("#city").val("")
+        $("#error").text("");
+
+        requestWeatherApi(city);
     });
 });
